fc35da1ed96b2243d2fc07da4fb1df8faff41593
Michael McGrew <mcgrew@mcgrew-mbp.local>
2016-01-06 14:55:18 -0800

    Adding requested changes

--- cb/watchlist_sharing/watchlist_importer.py
+++ cb/watchlist_sharing/watchlist_importer.py
@@ -2,39 +2,54 @@ import os
 import logging
 import cbapi
 import json
-from optparse import OptionParser
 import urllib
+from cbapi.util.cli_helpers import main_helper
 
 """
 Imports the output from watchlist exporter
 """
 
 class ImportWatchlists():
-    def __init__(self, options):
-        self.options = options
-        self.cb = cbapi.CbApi(
-            options.cb_url,
-            token=options.cb_token,
-            ssl_verify=False)
+    def __init__(self, cb, args):
+        self.args = args
+        self.cb = cb
         self.watch_lists = []
+
+    def confirm(self, watch_list):
+        prompt = 'Export watchlist %s? y/n: ' % (watch_list.get('Name'))
+        while True:
+            answer = raw_input(prompt)
+            if not answer:
+                return True
+            if answer not in ['y', 'Y', 'n', 'N']:
+                print 'Please enter y or n.'
+                continue
+            if answer in ['Y', 'y']:
+                return True
+            if answer in ['N', 'n']:
+                return False
+
     def get_watchlists(self):
-        with open(self.options.wl_file) as f:
-            for line in f:
-                while True:
-                    try:
-                        watch_list = json.loads(line)
-                        self.watch_lists.append(watch_list)
-                        break
-                    except ValueError:
-                        # Not yet a complete JSON value
-                        line += next(f)
+        watchlist_file = open(self.args.get('input_file'), 'r')
+        data = watchlist_file.read()
+        json_data = json.loads(data)
+        for watch_list in json_data.get('Watchlists'):
+            # Only get that specific watchlist
+            if self.args.get('watchlists'):
+                if watch_list.get('Name') not in self.args.get('watchlists').split(","):
+                    continue
+            # If we're being selective, ask the user
+            if self.args.get('selective_import'):
+                answer = self.confirm(watch_list)
+                if not answer:
+                    continue
+            self.watch_lists.append(watch_list)
 
     def add_watchlists(self):
         for watch_list in self.watch_lists:
             wl_name = watch_list.get('Name')
             wl_type = watch_list.get('Type')
-            wl_url = watch_list.get('Search String').encode('utf-8')
-            wl_url = 'q=' + urllib.quote(wl_url)
+            wl_url = watch_list.get('SearchString').encode('utf-8')
             print("-> Adding watchlist %s" % wl_name)
             watchlist = self.cb.watchlist_add(
                 wl_type,
@@ -42,32 +57,18 @@ class ImportWatchlists():
                 wl_url)
             print("-> Watchlist added [id=%s]" % (watchlist['id']))
 
-parser = OptionParser()
-parser.add_option("--cb_url",
-                  action="store",
-                  type="string",
-                  dest="cb_url",
-                  help="URL to CB server")
-parser.add_option("--cb_token",
-                  action="store",
-                  type="string",
-                  dest="cb_token",
-                  help="Token for CB server")
-parser.add_option("--file",
-                  action="store",
-                  type="string",
-                  dest="wl_file",
-                  help="File with CB watchlists")
-(options, args) = parser.parse_args()
-
-if options.cb_url is None:
-    parser.error('Must supply CB URL')
-if options.cb_token is None:
-    parser.error('Must supply CB URL token')
-if options.wl_file is None:
-    parser.error('Must supply file')
-
-if __name__ == '__main__':
-    import_wl = ImportWatchlists(options)
+def main(cb, args):
+    import_wl = ImportWatchlists(cb, args)
     import_wl.get_watchlists()
     import_wl.add_watchlists()
+
+if __name__ == "__main__":
+    selective_import = ("-m", "--selective", "store_true", False, "selective_import", "Select what watchlsits to import")
+    input_file = ("-f", "--file", "store", False, "input_file", "Select what file holds watchlists")
+    watchlists = ("-w", "--watchlists", "store", False, "watchlists", "Specific watchlist(s) to import. Can be comma separated.")
+    main_helper(
+        "Imports watchlists from a sharable format",
+        main,
+        custom_required=[input_file],
+        custom_optional=[selective_import,watchlists])
+
